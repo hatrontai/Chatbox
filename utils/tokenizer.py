3a791551431e23be6d602514b8309ccc97096618
UIT-21520436 <21520436@gm.uit.edu.vn>
2025-10-17 10:26:22 +0700

    load file tokenizer.py

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,1 +1,43 @@
+class tokenizer:
+    def __init__(self, text: str, vocab: dict) -> None:
+        self.text = text
+        
+        if not isinstance(vocab, dict):
+           self.vocab = list(sorted(list(set(text))))
+        else:
+              self.vocab = vocab
+        
+        self.token_to_id = {token: idx for idx, token in enumerate(self.vocab)}
+        self.id_to_token = {idx: token for idx, token in enumerate(self.vocab)}
 
+        def split(self, text: str) -> list:
+            lines = text.split('\n')
+            tokens = []
+            for line in lines:
+                tokens.extend(list(line))
+                tokens.append('\n')
+            tokens = tokens[:-1]  # Remove the last added newline
+            return tokens
+        
+        def encode(self, text: str) -> list:
+            tokens = self.split(text)
+            token_ids = [self.token_to_id[token] for token in tokens]
+            return token_ids
+        
+        def decode(self, token_ids: list) -> str:
+            tokens = [self.id_to_token[token_id] for token_id in token_ids]
+            text = ''.join(tokens)
+            return text
+
+with open('../rawdata//data.txt', 'r') as file:
+    data = file.read()
+
+ENCODER = tokenizer(data, vocab=None)
+
+if __name__ == "__main__":
+    sample_text = "hi , how are you ?"
+    print("size of vocab:", len(ENCODER.vocab))
+    encoded = ENCODER.encode(sample_text)
+    print("Encoded:", encoded)
+    decoded = ENCODER.decode(encoded)
+    print("Decoded:", decoded)
